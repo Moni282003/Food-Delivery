4a6f7e92a184cad785743b690be0567ce0ac41f6
Moni282003 <monish.m2021@vitstudent.ac.in>
2024-05-04 18:18:10 +0530

    Alert Mess

--- delivery-app/src/context/Storecontext.jsx
+++ delivery-app/src/context/Storecontext.jsx
@@ -1,39 +1,52 @@
 import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
-export const Storecontext=createContext(null)
-const StorecontextProvider=(props)=>{
-  const [cartitems,setCartItem]=useState({})
-  const addToCart=(itemId)=>{
-     if(!cartitems[itemId])
-     {
-      setCartItem((prev)=>({...prev,[itemId]:1}))
-     }
-     else{
-      setCartItem((prev)=>({...prev,[itemId]:prev[itemId]+1}))
-     }
-  }
-  const removeCart=(itemId)=>{
-    setCartItem((prev)=>({...prev,[itemId]:prev[itemId]-1}))
-  }
-  const getTotalCartAmout=()=>{
-    let totalAmout=0;
-    for(const item in cartitems){
-    if(cartitems[item]>0)
-  {
-let itemInfo=food_list.find((product)=>product._id===item);
-totalAmout+=itemInfo.price*cartitems[item];
-  }}
-  return totalAmout
-  }
- 
-  const contextValue={
-     food_list,cartitems,setCartItem,addToCart,removeCart,getTotalCartAmout
-  } 
 
-  return(
+export const Storecontext = createContext(null);
+
+const StorecontextProvider = (props) => {
+  const [cartitems, setCartItem] = useState({});
+
+  const addToCart = (itemId) => {
+    if (!cartitems[itemId]) {
+      setCartItem((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      const isConfirmed = window.confirm("Do you want to add another item?");
+      if (isConfirmed) {
+        setCartItem((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+      }
+    }
+    alert("Item added to cart");
+  };
+
+  const removeCart = (itemId) => {
+    setCartItem((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
+  const getTotalCartAmout = () => {
+    let totalAmout = 0;
+    for (const item in cartitems) {
+      if (cartitems[item] > 0) {
+        let itemInfo = food_list.find((product) => product._id === item);
+        totalAmout += itemInfo.price * cartitems[item];
+      }
+    }
+    return totalAmout;
+  };
+
+  const contextValue = {
+    food_list,
+    cartitems,
+    setCartItem,
+    addToCart,
+    removeCart,
+    getTotalCartAmout,
+  };
+
+  return (
     <Storecontext.Provider value={contextValue}>
-        {props.children}
+      {props.children}
     </Storecontext.Provider>
-  )
-}
+  );
+};
+
 export default StorecontextProvider;
